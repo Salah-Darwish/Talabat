d5abc83404601afa1df3170da921fb2c37e24df6
Salah Darwish <100836109+Salah-Darwish@users.noreply.github.com>
2023-10-26 01:23:24 +0300

    insert onion pattern and update database by scope

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public  decimal Price { get; set; }
+        public  int BrandId { get; set; } // Forgien Key Column  => ProductBrand
+      public  int CategoryId { get; set; } // Forgien Key Column  => ProductCategory
+        // Navigational Properity -> ProductBrand
+        public ProductBrand Brand { get; set; }
+
+        // Navigational Properity -> ProductCategory
+        public ProductCategory Category { get; set; }
+    }
+}
--- Talbat/Program.cs
+++ Talbat/Program.cs
@@ -1,19 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+using Talabat.Repository.Data;
+
 namespace Talbat
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var WebApplicationWebApplicationBuilder= WebApplication.CreateBuilder(args);
+            var WebApplicationBuilder= WebApplication.CreateBuilder(args);
 
             // Add services to DI the container.
             #region Configure Services
-            WebApplicationWebApplicationBuilder.Services.AddControllers();
+          WebApplicationBuilder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            WebApplicationWebApplicationBuilder.Services.AddEndpointsApiExplorer();
-            WebApplicationWebApplicationBuilder.Services.AddSwaggerGen();
+           WebApplicationBuilder.Services.AddEndpointsApiExplorer();
+           WebApplicationBuilder.Services.AddSwaggerGen();
+            WebApplicationBuilder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(WebApplicationBuilder.Configuration.GetConnectionString("DefaultConnection")); 
+            });
             #endregion
-            var app = WebApplicationWebApplicationBuilder.Build();
+            var app = WebApplicationBuilder.Build();
+              using var scope = app.Services.CreateScope();
+                var services = scope.ServiceProvider;
+                var _dbContext = services.GetRequiredService<StoreContext>();
+            // Ask Clr for Creating Object from DbContext Explicitly 
+
+            var loggerFactory =services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                await _dbContext.Database.MigrateAsync(); // Update-Database
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex,"an error has been occured during apply the migration");  
+            }
+         
             #region Configure Kestrel Services
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
