515a2b9a9c2165319ea2496b002ff1a3f69ddc12
Salah Darwish <100836109+Salah-Darwish@users.noreply.github.com>
2023-11-06 02:39:26 +0200

    Insert( Server Error ,Not found EndPoint,CleanUp The ProgramCs file)

--- Talbat/Controllers/APIBasedController.cs
+++ Talbat/Controllers/APIBasedController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Talbat.Controllers
+namespace Talabat.APIs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
